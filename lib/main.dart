b36485c3ab84cd34b01446e27be19b22c70b01db
TDH9001 <mohamedyehia707@gmail.com>
2024-10-21 23:33:28 +0300

    addedColor

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class homePage extends StatelessWidget {
           MaterialButton(
             onPressed: () {},
             child: Text("damaging attack"),
+            color: Colors.black,
           ),
         ],
       ),
