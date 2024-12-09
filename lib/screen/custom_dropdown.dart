d72ea3bbbaaaf6df42b82fddab0b471923494721
TDH9001 <mohamedyehia707@gmail.com>
2024-12-09 23:34:22 +0200

    small push to fix

--- lib/screen/custom_dropdown.dart
+++ lib/screen/custom_dropdown.dart
@@ -1,83 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:grad_proj/screen/text_style.dart';
+import '../UI/text_style.dart';
 
 class CustomDropdownField<T> extends StatelessWidget {
-    final String label;
-    final String hintText;
-    final T? value;
-    final Color? fillColor;
-    final BorderSide? borderSide;
-    final bool? isMandatory;
-    final List<T> items;
-    final ValueChanged<T?>? onChanged;
+  final String label;
+  final String hintText;
+  final T? value;
+  final Color? fillColor;
+  final BorderSide? borderSide;
+  final bool? isMandatory;
+  final List<T> items;
+  final ValueChanged<T?>? onChanged;
 
-    const CustomDropdownField({
-        Key? key,
-        required this.label,
-        required this.hintText,
-        this.value,
-        this.fillColor,
-        this.borderSide,
-        this.isMandatory,
-        required this.items,
-        this.onChanged,
-    }) : super(key: key);
+  const CustomDropdownField({
+    Key? key,
+    required this.label,
+    required this.hintText,
+    this.value,
+    this.fillColor,
+    this.borderSide,
+    this.isMandatory,
+    required this.items,
+    this.onChanged,
+  }) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                Row(
-                    children: [
-                        Text(
-                            label,
-                            style: TextStyles.text,
-                        ),
-                        if (isMandatory == true)
-                            Text(
-                                "  ",
-                                style: TextStyle(
-                                    color: const Color(0xff7AB2D3),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                ),
-                            ),
-                    ],
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              label,
+              style: TextStyles.text,
+            ),
+            if (isMandatory == true)
+              Text(
+                "  ",
+                style: TextStyle(
+                  color: const Color(0xff7AB2D3),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
                 ),
-                SizedBox(height: 5),
-                DropdownButtonFormField<T>(
-                    value: value,
-                    hint: Text(
-                        hintText,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 16),
-                    ),
-                    items: items.map((item) {
-                        return DropdownMenuItem<T>(
-                            value: item,
-                            child: Text(
-                                item.toString(),
-                                style: TextStyle(fontSize: 16),
-                            ),
-                        );
-                    }).toList(),
-                    onChanged: onChanged,
-                    isExpanded: true,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: fillColor ?? const Color(0xFFF9FAFB),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: borderSide ?? BorderSide.none,
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: borderSide ?? BorderSide.none,
-                        ),
-                    ),
-                ),
-            ],
-        );
-    }
+              ),
+          ],
+        ),
+        SizedBox(height: 5),
+        DropdownButtonFormField<T>(
+          value: value,
+          hint: Text(
+            hintText,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16),
+          ),
+          items: items.map((item) {
+            return DropdownMenuItem<T>(
+              value: item,
+              child: Text(
+                item.toString(),
+                style: TextStyle(fontSize: 16),
+              ),
+            );
+          }).toList(),
+          onChanged: onChanged,
+          isExpanded: true,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: fillColor ?? const Color(0xFFF9FAFB),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: borderSide ?? BorderSide.none,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: borderSide ?? BorderSide.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
--- lib/screen/tableform_screen.dart
+++ lib/screen/tableform_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grad_proj/screen/orgappbar.dart';
 import 'navbar_screen.dart';
-import 'text_style.dart';
+import '../UI/text_style.dart';
 
 class TableformScreen extends StatefulWidget {
   const TableformScreen({super.key});
@@ -13,42 +13,70 @@ class TableformScreen extends StatefulWidget {
 class _TableformScreenState extends State<TableformScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
-  final List<String> headers = ['Day', 'Courses', 'Hour',];
+  final List<String> headers = [
+    'Day',
+    'Courses',
+    'Hour',
+  ];
   final List<List<String>> rows = [
-    ['Monday', 'Maths', 'M101', ],
-    ['Tuesday', 'Physics', 'P202',],
-    ['Wednesday', 'Chemistry', 'C303', ],
-    ['Thursday', 'Biology', 'B404', ],
-    ['Friday', 'English', 'E505', ],
-    ['starday', 'English', 'E505', ],
+    [
+      'Monday',
+      'Maths',
+      'M101',
+    ],
+    [
+      'Tuesday',
+      'Physics',
+      'P202',
+    ],
+    [
+      'Wednesday',
+      'Chemistry',
+      'C303',
+    ],
+    [
+      'Thursday',
+      'Biology',
+      'B404',
+    ],
+    [
+      'Friday',
+      'English',
+      'E505',
+    ],
+    [
+      'starday',
+      'English',
+      'E505',
+    ],
   ];
 
   // Helper function to build a table row
   TableRow buildTableRow(List<String> cells, {bool isHeader = false}) {
     return TableRow(
-      decoration: isHeader
-          ? const BoxDecoration(color: Color(0xff7AB2D3))
-          : null,
+      decoration:
+          isHeader ? const BoxDecoration(color: Color(0xff7AB2D3)) : null,
       children: cells
           .map(
             (cell) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            cell,
-            style: isHeader
-                ? const TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.white)
-                : const TextStyle(),
-          ),
-        ),
-      )
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                cell,
+                style: isHeader
+                    ? const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white)
+                    : const TextStyle(),
+              ),
+            ),
+          )
           .toList(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( key:  scaffoldKey,
+    return Scaffold(
+      key: scaffoldKey,
       appBar: Orgappbar(scaffoldKey: scaffoldKey),
       drawer: const NavbarScreen(),
       body: Padding(
@@ -68,7 +96,6 @@ class _TableformScreenState extends State<TableformScreen> {
                     0: FixedColumnWidth(100),
                     1: FlexColumnWidth(100),
                     2: FlexColumnWidth(100),
-
                   },
                   children: [
                     buildTableRow(headers, isHeader: true),
