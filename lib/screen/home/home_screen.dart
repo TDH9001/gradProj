bc473da5fc6cc0eaca68c8ea5d32764a9f88e6d5
SarahMedhat54 <sarahmedhat820@gmail.com>
2025-02-20 23:43:28 +0200

    new

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -4,7 +4,10 @@ import 'package:grad_proj/screen/auth/login_screen.dart';
 import 'package:grad_proj/services/DB-service.dart';
 import 'package:grad_proj/services/navigation_Service.dart';
 import 'package:grad_proj/widgets/primary_button.dart';
+import 'package:grad_proj/widgets/sceduleitem.dart';
 import 'package:provider/provider.dart';
+
+import '../../UI/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -18,14 +21,75 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     var _auth = Provider.of<AuthProvider>(context);
     return Scaffold(
-      // body: Center(
-      //   child: PrimaryButton(
-      //       buttontext: "LOGOUT",
-      //       func: () {
-      //         _auth.signOut();
-      //         navigationService.instance.navigateToReplacement(LoginScreen.id);
-      //       }),
-      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CustomScrollView(
+          slivers:<Widget> [
+            SliverAppBar(
+              //expandedHeight: 120.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("chat"),
+              ),
+              backgroundColor: Color(0xff769BC6),
+        ),
+        SliverList(delegate: SliverChildBuilderDelegate(
+          childCount: 1 ,
+            (context, index) {
+              return Padding(padding:
+              const EdgeInsets.only(bottom: 20),
+                child: Sceduleitem(
+                  title: "User Name",
+                  subtitle: "phone number",
+                  thirdText: "the second",
+                ),
+              );
+            }
+        ),
+        ),
+          SliverList(delegate: SliverChildBuilderDelegate(
+              childCount: 1 ,
+                  (context, index) {
+                return Padding(padding:
+                const EdgeInsets.only(bottom: 20),
+                  child: Sceduleitem(
+                    title: "lab is",
+                    subtitle: "time is",
+                    thirdText: "where?",
+                  ),
+                );
+              }
+          ),
+          ),
+            SliverList(delegate: SliverChildBuilderDelegate(
+                childCount: 1 ,
+                    (context, index) {
+                  return Padding(padding:
+                  const EdgeInsets.only(bottom: 20),
+                    child: Sceduleitem(
+                      title: "lab is",
+                      subtitle: "time is",
+                      thirdText: "where?",
+                    ),
+                  );
+                }
+            ),
+
+
+
+
+
+        // body: Center(
+        //   child: PrimaryButton(
+        //       buttontext: "LOGOUT",
+        //       func: () {
+        //         _auth.signOut();
+        //         navigationService.instance.navigateToReplacement(LoginScreen.id);
+        //       }),
+        // ),
+        ),
+        ],
+            ),
+      ),
     );
   }
 }
