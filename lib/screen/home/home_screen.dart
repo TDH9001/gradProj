4f5826e8acb06aa5192c8a972ec76fd334276a62
TDH9001 <mohamedyehia707@gmail.com>
2025-03-01 09:24:13 +0200

    tested adding and removing scedule items from both users and courses

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:grad_proj/models/schedule.dart';
 import 'package:grad_proj/providers/auth_provider.dart';
 import 'package:grad_proj/screen/auth/login_screen.dart';
 import 'package:grad_proj/services/DB-service.dart';
@@ -22,74 +24,46 @@ class _HomeScreenState extends State<HomeScreen> {
     var _auth = Provider.of<AuthProvider>(context);
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CustomScrollView(
-          slivers:<Widget> [
-            SliverAppBar(
-              //expandedHeight: 120.0,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text("chat"),
-              ),
-              backgroundColor: Color(0xff769BC6),
-        ),
-        SliverList(delegate: SliverChildBuilderDelegate(
-          childCount: 1 ,
-            (context, index) {
-              return Padding(padding:
-              const EdgeInsets.only(bottom: 20),
-                child: Sceduleitem(
-                  title: "User Name",
-                  subtitle: "phone number",
-                  thirdText: "the second",
-                ),
-              );
-            }
-        ),
-        ),
-          SliverList(delegate: SliverChildBuilderDelegate(
-              childCount: 1 ,
-                  (context, index) {
-                return Padding(padding:
-                const EdgeInsets.only(bottom: 20),
-                  child: Sceduleitem(
-                    title: "lab is",
-                    subtitle: "time is",
-                    thirdText: "where?",
-                  ),
-                );
-              }
-          ),
-          ),
-            SliverList(delegate: SliverChildBuilderDelegate(
-                childCount: 1 ,
-                    (context, index) {
-                  return Padding(padding:
-                  const EdgeInsets.only(bottom: 20),
-                    child: Sceduleitem(
-                      title: "lab is",
-                      subtitle: "time is",
-                      thirdText: "where?",
-                    ),
-                  );
-                }
-            ),
-
-
-
-
-
-        // body: Center(
-        //   child: PrimaryButton(
-        //       buttontext: "LOGOUT",
-        //       func: () {
-        //         _auth.signOut();
-        //         navigationService.instance.navigateToReplacement(LoginScreen.id);
-        //       }),
-        // ),
-        ),
-        ],
-            ),
-      ),
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              PrimaryButton(
+                  buttontext: "addCourse",
+                  func: () {
+                    ScheduleItemClass scl = ScheduleItemClass(
+                      creatorId: "123",
+                      creatorName: "tester",
+                      day: 1,
+                      location: "nowhere",
+                      name: "test of type1",
+                      startTime: 1,
+                      type: 3,
+                      endTime: 1,
+                      //  endDate: Timestamp.now()
+                    );
+                    DBService.instance.addSceduleItem(
+                        "52stEYElBRO21mDdf85KdAaSg1E3", "math 105", scl);
+                  }),
+              PrimaryButton(
+                  //52stEYElBRO21mDdf85KdAaSg1E3
+                  buttontext: "removeCourse",
+                  func: () {
+                    ScheduleItemClass scl = ScheduleItemClass(
+                      creatorId: "123",
+                      creatorName: "tester",
+                      day: 1,
+                      location: "nowhere",
+                      name: "test of type1",
+                      startTime: 1,
+                      type: 3,
+                      endTime: 1,
+                      //  endDate: Timestamp.now()
+                    );
+                    DBService.instance.removeSceduleItem(
+                        scl, "52stEYElBRO21mDdf85KdAaSg1E3", "math 105");
+                  })
+            ],
+          )),
     );
   }
 }
