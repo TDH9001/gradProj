91308dbad6105ec55a9d3050706d05a0558dcfba
TDH9001 <mohamedyehia707@gmail.com>
2025-05-03 18:30:28 +0300

    finished refactoring chat page

--- lib/screen/chats/Chat_page.dart
+++ lib/screen/chats/Chat_page.dart
@@ -1,25 +1,15 @@
-import 'dart:async';
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:audioplayers/audioplayers.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:grad_proj/models/message.dart';
 import 'package:grad_proj/screen/chats/chat_data_screen.dart';
-import 'package:grad_proj/screen/chats/chat_page_widgets/image_message_button.dart';
+import 'package:grad_proj/screen/chats/chat_page_widgets/chat_page_message_field.dart';
 import 'package:grad_proj/screen/chats/chat_page_widgets/message_list_view_chat_lsit.dart';
-import 'package:grad_proj/services/caching_service/hive_cashing_service.dart';
-import 'package:grad_proj/services/cloud_Storage_Service.dart';
 import 'package:grad_proj/services/media_service.dart';
 import 'package:grad_proj/services/navigation_Service.dart';
 import 'package:grad_proj/services/snackbar_service.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:record/record.dart';
-import 'package:uuid/uuid.dart';
 import '../../theme/light_theme.dart';
 import '../../UI/text_style.dart';
-import '../../services/DB-service.dart';
 import '../../providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -32,7 +22,7 @@ class ChatPage extends StatefulWidget {
   final ScrollController _LVC = ScrollController();
   List<String> admins;
   final TextEditingController txt = TextEditingController();
-  final record = AudioRecorder();
+  //final record = AudioRecorder();
   late AudioPlayer audioPlayer = AudioPlayer();
   // PlatformDispatcher.
 
@@ -56,32 +46,6 @@ class _ChatPageState extends State<ChatPage> {
 
     //  _audioPlayer = AudioPlayer();
     // Call the method during initialization
-  }
-
-  void startRecord(AudioRecorder rec) async {
-    var location = await getApplicationDocumentsDirectory();
-    String fileName = Uuid().v4();
-    if (await rec.hasPermission()) {
-      await rec.start(RecordConfig(), path: location.path + fileName + '.m4a');
-      setState(() {
-        isRecording = true;
-      });
-    }
-  }
-
-  Future<String?> stopRecord(AudioRecorder rec) async {
-    String? finalPath = await rec.stop();
-    setState(() {
-      isRecording = false;
-    });
-    if (finalPath != null) {
-      var _result = await CloudStorageService.instance.uploadVoice(
-          uid: AuthProvider.instance.user!.uid, fileData: File(finalPath));
-      return await _result!.ref.getDownloadURL();
-    } else {
-      SnackBarService.instance
-          .showsSnackBarError(text: "could not uplaod the file");
-    }
   }
 
   @override
@@ -120,111 +84,17 @@ class _ChatPageState extends State<ChatPage> {
             chatID: widget.chatID,
           ),
           Align(
-              child: MessageField(context), alignment: Alignment.bottomCenter),
+              alignment: Alignment.bottomCenter,
+              child: MessageField(
+                GK: widget.GK,
+                chatID: widget.chatID,
+                admins: widget.admins,
+                isRecording: isRecording,
+                txt: widget.txt,
+              )),
         ],
       );
     });
-  }
-
-  Widget MessageField(BuildContext _context) {
-    return Container(
-      // height: _height * 0.1,
-      width: _width,
-      decoration: BoxDecoration(
-          color: Colors.grey[200], borderRadius: BorderRadius.circular(22)),
-      margin: EdgeInsets.symmetric(
-          horizontal: _width * 0.02, vertical: _height * 0.02),
-      child: Form(
-          key: widget.GK,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: widget.admins
-                    .contains(HiveCashingService.getUserContactData().id.trim())
-                ? [
-                    _messageTextField(widget.txt),
-                    _sendMessageButton(_context, widget.txt),
-                    ImageMessageButton(
-                      chatID: widget.chatID,
-                    )
-                  ]
-                : [
-                    Text("only admins can contribute to this chat"),
-                  ],
-          )),
-    );
-  }
-
-  Widget _messageTextField(final TextEditingController txt) {
-    return SizedBox(
-      width: _width * 0.5,
-      child: TextFormField(
-        controller: txt,
-        onTap: () {
-          txt.text += " ";
-        },
-        validator: (data) {
-          if (data == null || data.trim().isEmpty) {
-            return "The message cannot be empty";
-          }
-          return null;
-        },
-        cursorColor: Colors.black,
-        autocorrect: false,
-        decoration: InputDecoration(
-            border: InputBorder.none, hintText: "type a Massage ... "),
-      ),
-    );
-  }
-
-  Widget _sendMessageButton(
-    BuildContext context,
-    TextEditingController txt,
-  ) {
-    return SizedBox(
-      // height: _height * 0.1,
-      width: _width * 0.09,
-      child: IconButton(
-        icon: Icon(
-            widget.txt.text.isEmpty
-                ? (isRecording ? Icons.stop : Icons.mic)
-                : Icons.send,
-            color: LightTheme.primary),
-        onPressed: () async {
-          if (widget.txt.text.isEmpty) {
-            if (isRecording) {
-              startRecord(widget.record);
-            } else {
-              String? VoiceUrl = await stopRecord(widget.record);
-              DBService.instance.addMessageInChat(
-                  chatId: widget.chatID,
-                  messageData: Message(
-                      senderID: HiveCashingService.getUserContactData().id,
-                      messageContent: VoiceUrl!,
-                      timestamp: Timestamp.now(),
-                      type: "voice",
-                      senderName:
-                          "${HiveCashingService.getUserContactData().firstName} ${HiveCashingService.getUserContactData().lastName}"));
-            }
-          } else if (widget.GK.currentState!.validate() &&
-              widget.txt.text.isNotEmpty) {
-            // txt.text.trim();
-            DBService.instance.addMessageInChat(
-                chatId: widget.chatID,
-                messageData: Message(
-                    senderID: HiveCashingService.getUserContactData().id,
-                    messageContent: txt.text.trim(),
-                    timestamp: Timestamp.now(),
-                    type: "text",
-                    senderName:
-                        "${HiveCashingService.getUserContactData().firstName} ${HiveCashingService.getUserContactData().lastName}"));
-          }
-          txt.text = "";
-          FocusScope.of(context).unfocus();
-        },
-      ),
-    );
   }
 }
 
---
+++ lib/screen/chats/chat_page_widgets/send_message_button.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:grad_proj/models/message.dart';
+import 'package:grad_proj/services/DB-service.dart';
+import 'package:grad_proj/services/caching_service/hive_cashing_service.dart';
+import 'package:grad_proj/services/cloud_Storage_Service.dart';
+import 'package:grad_proj/services/media_service.dart';
+import 'package:grad_proj/services/snackbar_service.dart';
+import 'package:grad_proj/theme/light_theme.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
+import 'package:uuid/uuid.dart';
+
+class SendMessageButton extends StatefulWidget {
+  SendMessageButton(
+      {super.key,
+      //  required BuildContext context,
+      required this.txt,
+      required this.isRecording,
+      required this.chatID,
+      required this.GK});
+  final TextEditingController txt;
+  bool isRecording;
+  final record = AudioRecorder();
+  final String chatID;
+  GlobalKey<FormState> GK;
+
+  @override
+  State<SendMessageButton> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SendMessageButton> {
+  void startRecord(AudioRecorder rec) async {
+    var location = await getApplicationDocumentsDirectory();
+    String fileName = Uuid().v4();
+    if (await rec.hasPermission()) {
+      await rec.start(RecordConfig(), path: location.path + fileName + '.m4a');
+      setState(() {
+        widget.isRecording = true;
+      });
+    }
+  }
+
+  Future<String?> stopRecord(AudioRecorder rec) async {
+    String? finalPath = await rec.stop();
+    setState(() {
+      widget.isRecording = false;
+    });
+    if (finalPath != null) {
+      var _result = await CloudStorageService.instance.uploadVoice(
+          uid: HiveCashingService.getUserContactData().id,
+          fileData: File(finalPath));
+      return await _result!.ref.getDownloadURL();
+    } else {
+      SnackBarService.instance
+          .showsSnackBarError(text: "could not uplaod the file");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SnackBarService.instance.buildContext = context;
+    return SizedBox(
+      // height: _height * 0.1,
+      width: MediaService.instance.getWidth() * 0.09,
+      child: IconButton(
+        icon: Icon(
+            widget.txt.text.isEmpty
+                ? (widget.isRecording ? Icons.stop : Icons.mic)
+                : Icons.send,
+            color: LightTheme.primary),
+        onPressed: () async {
+          if (widget.txt.text.isEmpty) {
+            if (!widget.isRecording) {
+              startRecord(widget.record);
+            } else {
+              String? VoiceUrl = await stopRecord(widget.record);
+              DBService.instance.addMessageInChat(
+                  chatId: widget.chatID,
+                  messageData: Message(
+                      senderID: HiveCashingService.getUserContactData().id,
+                      messageContent: VoiceUrl!,
+                      timestamp: Timestamp.now(),
+                      type: "voice",
+                      senderName:
+                          "${HiveCashingService.getUserContactData().firstName} ${HiveCashingService.getUserContactData().lastName}"));
+            }
+          } else if (widget.GK.currentState!.validate() &&
+              widget.txt.text.isNotEmpty) {
+            // txt.text.trim();
+            DBService.instance.addMessageInChat(
+                chatId: widget.chatID,
+                messageData: Message(
+                    senderID: HiveCashingService.getUserContactData().id,
+                    messageContent: widget.txt.text.trim(),
+                    timestamp: Timestamp.now(),
+                    type: "text",
+                    senderName:
+                        "${HiveCashingService.getUserContactData().firstName} ${HiveCashingService.getUserContactData().lastName}"));
+          }
+          widget.txt.text = "";
+          FocusScope.of(context).unfocus();
+        },
+      ),
+    );
+  }
+}
