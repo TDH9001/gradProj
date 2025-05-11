bb0a47842ae3a6e817ae027a783990f2046fec73
SarahMedhat54 <sarahmedhat820@gmail.com>
2025-05-11 21:03:26 +0300

    pdf screen is finish

--- lib/screen/pdf/student_guide_screen.dart
+++ lib/screen/pdf/student_guide_screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:grad_proj/widgets/orgappbar.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
@@ -9,7 +10,7 @@ class StudentGuideScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Orgappbar(scaffoldKey: scaffoldKey, title: "Student Guide",),
+      appBar: Orgappbar(scaffoldKey: scaffoldKey, title: 'Pdf.title'.tr(),),
       body:SfPdfViewer.asset('assets/pdf/student_guide.pdf'),
     );
   }
