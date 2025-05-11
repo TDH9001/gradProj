ceab6c1cda352a6fdccbca36ef3810328e23a6e3
SarahMedhat54 <sarahmedhat820@gmail.com>
2025-05-11 19:20:14 +0300

    up
    edit updatedSceduleItem ,Orgappbar

--- lib/widgets/orgappbar.dart
+++ lib/widgets/orgappbar.dart
@@ -4,63 +4,48 @@ import '../UI/text_style.dart';
 import '../providers/theme_provider.dart';
 import '../screen/theme/dark_theme_colors.dart';
 import '../screen/theme/light_theme.dart';
-
 class Orgappbar extends StatelessWidget implements PreferredSizeWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
   final String title;
-  final VoidCallback? onBack;
+  final Widget? leading;
 
   Orgappbar({
     required this.scaffoldKey,
     Key? key,
     this.title = 'SciConnect',
-    this.onBack,
+    this.leading,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     final bool isDarkMode = themeProvider.isDarkMode;
-    final bool isRTL = Directionality.of(context) == TextDirection.rtl;
-    final backIcon = IconButton(
-      icon: Icon(
-        isRTL ? Icons.arrow_forward : Icons.arrow_back,
-        color: Colors.white,
-      ),
-      onPressed: onBack ?? () => Navigator.pop(context),
-    );
-    final image = Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: CircleAvatar(
-        backgroundColor: isDarkMode
-            ? DarkThemeColors.backgroundImage
-            : LightTheme.backgroundImage,
-        backgroundImage: const AssetImage('assets/images/science.png'),
-        radius: 20,
-      ),
-    );
-
-    return AppBar(
-      backgroundColor: isDarkMode
-          ? DarkThemeColors.primary
-          : LightTheme.primary,
-      automaticallyImplyLeading: false,
-
-      title: Center(
-        child: Text(
-          title,
-          style: TextStyles.appBarText.copyWith(
-            color: Colors.white,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: AppBar(
+        backgroundColor: isDarkMode ?DarkThemeColors.primary: LightTheme.primary,
+        //Color(0xFF2E5077)
+        title: Center (
+          child: Text(
+            title,
+            style: TextStyles.appBarText.copyWith(
+              color:Colors.white,
+            ),
           ),
         ),
+        leading: leading,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundColor: isDarkMode ? DarkThemeColors.backgroundImage: LightTheme.backgroundImage,
+              backgroundImage: const AssetImage('assets/images/science.png'),
+              radius: 20,
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
       ),
-
-
-      leading: isRTL ? image : backIcon,
-      actions: [
-        if (isRTL) backIcon else image,
-        const SizedBox(width: 10),
-      ],
     );
   }
 
--- lib/widgets/updated_scedule_item.dart
+++ lib/widgets/updated_scedule_item.dart
@@ -6,97 +6,131 @@ import 'package:timeago/timeago.dart' as timeago;
 import '../providers/theme_provider.dart';
 
 Widget updatedSceduleItem(ScheduleItemClass scl) {
- //final themeProvider = Provider.of<ThemeProvider>(context);
-  //final bool isDarkMode = themeProvider.isDarkMode;
-//  final ScheduleItemClass scl;
-  // print(scl.endDate.toString());
-  // if (scl.type == 2) {
-  //   print(scl.endDate!.toDate());
-  // }
-  // print("////////////////////////");
+  LinearGradient getGradient(int type) {
+    switch (type) {
+      case 1:
+        return const LinearGradient(
+          colors: [Color(0xff2E5077), Color(0xff769BC6)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        );
+      case 2:
+        return const LinearGradient(
+          colors: [Color(0xFFD9534F), Color(0xFFFA8072)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        );
+      default:
+        return const LinearGradient(
+          colors: [Color(0xFF5CB85C), Color(0xFF8BC34A)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        );
+    }
+  }
 
-  return Container(
-    decoration: BoxDecoration(
-      color: scl.type == 1
-          ? const Color(0xFF4A739F)
-          : scl.type == 2
-              ?  Color(0xffF44336)
-              : Color(0xFF388E3C),
-      borderRadius: BorderRadius.circular(12),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.2),
-          blurRadius: 6,
-          offset: const Offset(0, 3),
-        ),
-      ],
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                scl.name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                scl.location,
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                scl.creatorName,
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Text(
-                "${(scl.startTime / 100).floor() % 12}:${scl.startTime % 100} ${scl.startTime / 100 > 12 ? "PM" : "AM"} - ${(scl.endTime / 100).floor() % 12}:${scl.endTime % 100} ${scl.endTime / 100 > 12 ? "PM" : "AM"}",
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Text(
-                "${days.values[scl.day].name}",
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              scl.endDate != null
-                  ? Text(
-                      //needs to be made correct
-                      timeago.format(scl.endDate!.toDate(),
-                          allowFromNow: true),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    )
-                  : SizedBox()
-            ],
+  IconData getIcon(int type) {
+    switch (type) {
+      case 1:
+        return Icons.school;
+      case 2:
+        return Icons.warning_amber_rounded;
+      default:
+        return Icons.check_circle_outline;
+    }
+  }
+
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+    child: Container(
+      decoration: BoxDecoration(
+        gradient: getGradient(scl.type),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 5,
+            offset: const Offset(0, 3),
           ),
         ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      getIcon(scl.type),
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      scl.name,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  scl.location,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  scl.creatorName,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "${(scl.startTime / 100).floor() % 12}:${scl.startTime % 100} ${scl.startTime / 100 > 12 ? "PM" : "AM"} - ${(scl.endTime / 100).floor() % 12}:${scl.endTime % 100} ${scl.endTime / 100 > 12 ? "PM" : "AM"}",
+                  style: const TextStyle(
+                    fontSize: 11,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  days.values[scl.day].name,
+                  style: const TextStyle(
+                    fontSize: 11,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                scl.endDate != null
+                    ? Text(
+                  timeago.format(scl.endDate!.toDate(),
+                      allowFromNow: true),
+                  style: const TextStyle(
+                    fontSize: 11,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+                    : const SizedBox()
+              ],
+            ),
+          ],
+        ),
       ),
     ),
   );
